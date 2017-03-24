dcfc86c15b15f2a4193639522bc04b6fc14bd1d9
Demis Gomes <demis.gomes@gprt.ufpe.br>
2017-03-24 13:16:22 -0300

    Questions retrieved sucessfully. Creation of a answers file.

--- src/socket/ShowMilhaoImplementation.java
+++ src/socket/ShowMilhaoImplementation.java
@@ -13,27 +13,115 @@ import domain.Question;
 public class ShowMilhaoImplementation implements IShowMilhao {
 	
 	//nome do arquivo de perguntas
-	private final String FILENAME="questions.txt";
+	private final String QUESTIONS_FILENAME="questions.txt";
+	private final String ANSWERS_FILENAME="answers.txt";
 	
 	public HashMap<Integer,Question> importQuestions(){
 		HashMap <Integer, Question> questions=new HashMap<>();
+		int questionId=0;
 		try {
+			//criar metodo pra isso
 			  String dir=System.getProperty("user.dir");
-			  File file = new File(dir+"/src/config/"+FILENAME);
+			  File file = new File(dir+"/src/config/"+QUESTIONS_FILENAME);
 		      FileReader fileReader=new FileReader(file);
-		      BufferedReader lerArq = new BufferedReader(fileReader);
+		      BufferedReader readFile = new BufferedReader(fileReader);
 		 
-		      String linha = lerArq.readLine(); // lê a primeira linha
-		// a variável "linha" recebe o valor "null" quando o processo
-		// de repetição atingir o final do arquivo texto
-		      while (linha != null) {
-		        System.out.printf("%s\n", linha);
+		      String line = readFile.readLine(); // lï¿½ a primeira linha
+		// a variï¿½vel "linha" recebe o valor "null" quando o processo
+		// de repetiï¿½ï¿½o atingir o final do arquivo texto
+		      line = readFile.readLine(); // lï¿½ da segunda atï¿½ a ï¿½ltima linha
+		       
+		        
+		      while (line != null) {
+		       // System.out.printf("%s\n", linha);
 		 
-		        linha = lerArq.readLine(); // lê da segunda até a última linha
+		        if (line==null){
+		        	continue;
+		        }
+		        Question question = new Question();
+			    HashMap<Integer,String> answers=new HashMap<>();
+			    
+			   
+		        
+		        if (line.length()==0){
+		        	//se a linha nao estiver em branco e nem identificar a dificuldade das questoes
+			        //eh uma questao
+		        	//incrementa o id da questao
+		        	
+		        	do{
+		        		line=readFile.readLine();
+			        	//le a linha
+		        		if (line==null){
+		        			break;
+		        		}
+		        		if (line.length()==0){
+		        			continue;
+		        		}
+		        		if (line.equals("### FÃ¡cil") || line.equals("### MÃ©dio") || line.equals("### DifÃ­cil")){
+		        			continue;
+		        		}
+		        		
+		        		
+		        		if (line.equals("### Answers")){
+		 			    	line=readFile.readLine();
+		 			    	break;
+		 			    }
+		        		
+		        		else{
+		        			//String [] splittedLine=linha.split("\n");
+			        		String firstLetter=line.substring(0,1);
+			        		//System.out.println(firstLetter);
+			        		//System.out.println(firstLetter.equals("1"));
+			        		if (!firstLetter.equals("1") && !firstLetter.equals("2") && !firstLetter.equals("3") && !firstLetter.equals("4")){
+			        			questionId++;
+				        		question.setTitle(line);
+				        		//System.out.println(question.getTitle());
+				        	}
+				        	//perguntas
+				        	else{
+				        		//System.out.println("hora do else");
+				        		//System.out.println(linha);
+				        		int answerId=Integer.parseInt(line.substring(0,1));
+				        		String answer=line.substring(2);
+				        		answers.put(answerId, answer);
+				        	}
+			        	}
+		        		
+		        	}
+		        	while (line.length()>0);
+		        	question.setAnswers(answers);
+		        	questions.put(questionId, question);
+		     
+		        }
+		        
 		      }
 		 
 		      fileReader.close();
-		    } catch (IOException e) {
+		      readFile.close();
+		      
+		      //criar metodo pra isso
+		      File fileAnswers = new File(dir+"/src/config/"+ANSWERS_FILENAME);
+		      FileReader fileReaderAnswers=new FileReader(fileAnswers);
+		      BufferedReader readAns = new BufferedReader(fileReaderAnswers);
+		      
+		      String lineAnswer=readAns.readLine();
+		      if (lineAnswer.equals("### Answers")){
+
+			    	lineAnswer=readAns.readLine();
+			    	lineAnswer=readAns.readLine();
+
+			    	for (int i=1;i<questions.size()+1;i++){
+			    		int correctAnswerId=Integer.parseInt(lineAnswer);
+			    		questions.get(i).setCorrectAnswer(correctAnswerId);
+			    		lineAnswer=readAns.readLine();
+			    	}	
+		        	
+		        }
+		      
+		      readAns.close();
+		      fileReaderAnswers.close();
+		      
+		     } catch (IOException e) {
 		        System.err.printf("Erro na abertura do arquivo: %s.\n",
 		          e.getMessage());
 		    }
